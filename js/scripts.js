d2621050bf3df661b4dc11f18deeb9f9f7555c00
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-02-20 10:28:04 -0800

    Return 6 slices if the pizza is between 8 and 12 inches.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ var Pizza = {
     if ((4 <= this.size) && (this.size < 8)) {
       this.pie += 4;
     }
+    if ((8 <= this.size) && (this.size < 12)) {
+      this.pie += 6;
+    }
     return this.pie;
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,14 @@
 describe("Pizza", function() {
   describe("slice", function() {
-    it("will return four slices if the diameter is greater than 4 and less than 8 inches", function() {
+    it("will return four slices if the diameter is greater than or equal to 4 and less than 8 inches", function() {
       var testPizza = Object.create(Pizza);
       testPizza.size = 7;
       expect(testPizza.slice()).to.equal(4);
+    });
+    it("will return 6 slices if the diameter is between 8 and 12 inches", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.size = 10;
+      expect(testPizza.slice()).to.equal(6);
     });
   });
 });
